9b1e0a1da138a7a1f23fbd7f2d1223307489ed36
Yan-Ming Chiou <steveice@me.com>
2019-04-19 17:29:23 -0400

    Finishe the first part of the tutorial
    Next step is adding enemy on the game

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+
+        }
+
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        Debug.Log(exitBackgroundImageCanvasGroup.alpha);
+        //Debug.Log(m_Timer);
+
+        if(m_Timer> fadeDuration+ displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+        
+    }
+}
